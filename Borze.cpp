82daca0c1902b54bdfe86f702cc1180499ae3906
ishangupta-tech <ishangupta5706@gmail.com>
2026-06-25 19:09:15 +0530

    Add Borze.cpp for decoding algorithm
    Implement Borze decoding algorithm to convert input string.

---
+++ Borze.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    
+    for(int i = 0; i < s.length();) {
+        if(s[i] == '.') {
+            cout<< 0;
+            i++;
+        }
+        else if(s[i] == '-' && s[i+1] == '.') {
+            cout<< 1;
+            i += 2;
+        }
+        else {
+            cout<< 2;
+            i += 2;
+        }
+    }
+    return 0;
+}
